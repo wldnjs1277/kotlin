a963e4b23d370d136a837abe8e5c75ce89c6f31a
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-02 19:46:13 +0900

    add

--- app/src/main/java/com/example/test1/Sample1.kt
+++ app/src/main/java/com/example/test1/Sample1.kt
@@ -2,9 +2,13 @@ package com.example.test1
 
 fun main(){
     helloworld()
+    println(add(a=4,b=5))
 }
 //1.함수
 
 fun helloworld(){
     println("Hello world!")
+}
+fun add(a:Int, b:Int):Int{
+    return a+b
 }
