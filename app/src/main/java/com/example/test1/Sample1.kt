cd4361628cc162de8c7acf3aefb556341cc06d29
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-02 20:49:54 +0900

    조건식 if , when

--- app/src/main/java/com/example/test1/Sample1.kt
+++ app/src/main/java/com/example/test1/Sample1.kt
@@ -1,14 +1,57 @@
 package com.example.test1
 
 fun main(){
-    helloworld()
-    println(add(a=4,b=5))
+    // string Template
+    /*val name="jiwon"
+    val lastname = "Lee"
+    println("my name is ${name +" "+lastname}")
+    println("this is 2\$")*/
+    checknumber(1)
 }
-//1.함수
+//함수
 
 fun helloworld(){
     println("Hello world!")
 }
 fun add(a:Int, b:Int):Int{
     return a+b
+}
+fun hi(){
+    val a : Int= 5
+    var b : Int= 10
+    val c = 100
+    var d = 100
+    var name = "jiwon"
+    var e : String
+}
+//조건식
+
+fun maxBy(a :Int, b: Int) : Int {
+    if(a>b){
+        return a
+    }else{
+        return b
+    }
+}
+
+fun maxBy2(a:Int,b: Int) : Int = if(a>b) a else b
+
+fun checknumber(score : Int) {
+    when(score) {
+        0 -> println("this is 0")
+        1 -> println("this is 1")
+        2,3 -> println("this is 2 or 3")
+        else -> println("i don't know")
+    }
+    var b : Int = when(score){
+        1 -> 1
+        2 -> 2
+        else -> 3
+    }
+    println("b : ${b}")
+    when(score){
+        in 90..100 -> println("you are genius")
+        in 10..80 -> println("not bad")
+        else -> println("okay")
+    }
 }
